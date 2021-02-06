1e368bddfc6bbdd560097cfe7abe24fa68eaba4f
Joe Mancuso <idmann509@gmail.com>
2021-02-06 09:18:22 -0500

    started mailables

---
+++ src/masonite/mail/Mailable.py
@@ -0,0 +1,25 @@
+class Mailable:
+
+    def to(self, to: list):
+        pass
+
+    def from_(self, _from):
+        pass
+
+    def attach(self, name, attachment):
+        pass
+
+    def reply_to(self, name, attachment):
+        pass
+
+    def format(self, format):
+        pass
+
+    def ssl(self):
+        pass
+
+    def tls(self):
+        pass
+
+    def subject(self):
+        pass
