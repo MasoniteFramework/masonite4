addec9dd5d975b31ebbb8d4fe5aed8e1746dca98
Joe Mancuso <idmann509@gmail.com>
2021-02-07 10:02:36 -0500

    added ability to pass list

--- src/masonite/drivers/mail/Recipient.py
+++ src/masonite/drivers/mail/Recipient.py
@@ -1,5 +1,7 @@
 class Recipient:
     def __init__(self, recipient):
+        if isinstance(recipient, (list, tuple)):
+            recipient = ','.join(recipient)
         self.recipient = recipient
 
     def header(self):
