60b2924409f060161c9457812f03f13864b9982b
Samuel Girardin <samuelgirardin@pm.me>
2021-02-14 12:47:25 +0100

    refactor methods

--- tests/core/utils/test_location.py
+++ tests/core/utils/test_location.py
@@ -1,6 +1,12 @@
 from tests import TestCase
 
-from src.masonite.utils.location import view_path, controller_path
+from src.masonite.utils.location import (
+    view_path,
+    controller_path,
+    seeds_path,
+    migrations_path,
+    config_path
+)
 
 
 class TestLocation(TestCase):
@@ -19,3 +25,27 @@ class TestLocation(TestCase):
         self.assertTrue(location.endswith("tests/integrations/controllers/account/MyController.py"))
         location = controller_path("MyController.py", absolute=False)
         self.assertEqual("tests/integrations/controllers/MyController.py", location)
+
+    def test_config_path(self):
+        location = config_path("app.py")
+        self.assertTrue(location.endswith("tests/integrations/config/app.py"))
+        location = config_path("package/base.py")
+        self.assertTrue(location.endswith("tests/integrations/config/package/base.py"))
+        location = config_path("app.py", absolute=False)
+        self.assertEqual("tests/integrations/config/app.py", location)
+
+    def test_migrations_path(self):
+        location = migrations_path("create_users_table.py")
+        self.assertTrue(location.endswith("tests/integrations/databases/migrations/create_users_table.py"))
+        location = migrations_path("package/create_team_table.py")
+        self.assertTrue(location.endswith("tests/integrations/databases/migrations/package/create_team_table.py"))
+        location = migrations_path("create_users_table.py", absolute=False)
+        self.assertEqual("tests/integrations/databases/migrations/create_users_table.py", location)
+
+    def test_seeds_path(self):
+        location = seeds_path("create_users.py")
+        self.assertTrue(location.endswith("tests/integrations/databases/seeds/create_users.py"))
+        location = seeds_path("package/create_teams.py")
+        self.assertTrue(location.endswith("tests/integrations/databases/seeds/package/create_teams.py"))
+        location = seeds_path("create_users.py", absolute=False)
+        self.assertEqual("tests/integrations/databases/seeds/create_users.py", location)
