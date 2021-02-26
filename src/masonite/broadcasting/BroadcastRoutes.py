80b039f12e461c9b8d19bc8bd267ec2035889b7a
Joe Mancuso <idmann509@gmail.com>
2021-02-26 00:11:17 -0500

    cleaned code

--- src/masonite/broadcasting/BroadcastRoutes.py
+++ src/masonite/broadcasting/BroadcastRoutes.py
@@ -17,16 +17,3 @@ class BroadcastingController:
         return broadcast.driver("pusher").authorize(
             request.input("channel_name"), request.input("socket_id")
         )
-        # import pusher
-
-        # pusher_client = pusher.Pusher(
-        #     app_id=os.getenv("PUSHER_APP_ID"),
-        #     key=os.getenv("PUSHER_CLIENT"),
-        #     secret=os.getenv("PUSHER_SECRET"),
-        #     cluster=os.getenv("PUSHER_CLUSTER"),
-        #     ssl=False,
-        # )
-
-        # return pusher_client.authenticate(
-        #     channel=request.input("channel_name"), socket_id=request.input("socket_id")
-        # )
