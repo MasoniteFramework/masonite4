ee9946980e6358582d6fb6f98a4f53743143a561
Joe Mancuso <idmann509@gmail.com>
2021-03-01 22:30:08 -0500

    removed comments

--- tests/integrations/web.py
+++ tests/integrations/web.py
@@ -10,22 +10,5 @@ Route.get("/emit", "WelcomeController@emit"),
 Route.get("/view", "WelcomeController@view"),
 Route.get("/mail", "MailableController@view"),
 
-# Route.get("/login", "auth.LoginController@show").name("login")
-# Route.get("/home", "auth.HomeController@show").name("auth.home")
-# Route.get("/register", "auth.RegisterController@show").name("register")
-# Route.post("/register", "auth.RegisterController@store").name("register.store")
-# Route.get("/password_reset", "auth.PasswordResetController@show").name("password_reset")
-# Route.post("/password_reset", "auth.PasswordResetController@store").name(
-#     "password_reset.store"
-# )
-# Route.get("/change_password", "auth.PasswordResetController@change_password").name(
-#     "change_password"
-# )
-# Route.post(
-#     "/change_password", "auth.PasswordResetController@store_changed_password"
-# ).name("change_password.store")
-# Route.post("/login", "auth.LoginController@store").name("login.store")
-# Route.post("/login", "auth.LoginController@store").name("login.store")
-
 Broadcast.routes()
 Auth.routes()
