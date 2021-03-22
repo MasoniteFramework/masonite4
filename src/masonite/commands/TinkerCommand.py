fe42e49cf4767492bb0875a5b068110c6a6900ab
Samuel Girardin <samuelgirardin@pm.me>
2021-03-22 12:50:31 +0100

    add ipython option

--- src/masonite/commands/TinkerCommand.py
+++ src/masonite/commands/TinkerCommand.py
@@ -16,6 +16,7 @@ class TinkerCommand(Command):
     Run a python shell with the container pre-loaded.
 
     tinker
+        {--i|ipython : Run a IPython shell}
     """
 
     def handle(self):
@@ -26,4 +27,22 @@ class TinkerCommand(Command):
         )
         banner = BANNER.format(version)
 
-        code.interact(banner=banner, local={"app": application})
+        if self.option("ipython"):
+
+            try:
+                import IPython
+            except ImportError:
+                raise ModuleNotFoundError(
+                    "Could not find the 'IPython' library. Run 'pip install ipython' to fix this."
+                )
+            from traitlets.config import Config
+
+            c = Config()
+            # c.InteractiveShellApp.exec_lines = [
+            #     'print("\\nimporting some things\\n")',
+            # ]
+            c.TerminalInteractiveShell.banner1 = banner
+            context = {"app": application}
+            IPython.start_ipython(argv=[], user_ns=context, config=c)
+        else:
+            code.interact(banner=banner, local={"app": application})
