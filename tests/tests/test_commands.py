dbd3dd1bfe4707e37e034858cc0d5a9e3ab9fbd2
Samuel Girardin <samuelgirardin@pm.me>
2021-05-03 20:15:53 +0200

    use help command for testing

--- tests/tests/test_commands.py
+++ tests/tests/test_commands.py
@@ -3,22 +3,17 @@ from tests import TestCase
 
 class TestCommandsAssertions(TestCase):
     def test_running_command_during_tests(self):
-        self.craft("key")
+        self.craft("help")
 
     def test_assert_output(self):
-        self.craft("key").assertOutputContains("Key added to your .env file:")
-
-        # this command prints also key value
-        with self.assertRaises(AssertionError):
-            self.craft("key").assertExactOutput("Key added to your .env file:")
+        self.craft("help").assertOutputContains("Masonite Version: version 4.0")
 
     def test_assert_output_missing(self):
-        self.craft("key").assertOutputMissing("This is not in output")
+        self.craft("help").assertOutputMissing("This is not in the help")
 
     def test_assert_errors(self):
-        # this commands has no errors
         with self.assertRaises(AssertionError):
-            self.craft("key").assertHasErrors()
+            self.craft("help").assertHasErrors()
 
     def test_assert_success(self):
-        self.craft("key").assertSuccess()
+        self.craft("help").assertSuccess()
