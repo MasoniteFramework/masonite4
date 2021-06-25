3fb268af2d3003626894bb13250fe5799eb9ca5a
Joe Mancuso <idmann509@gmail.com>
2021-06-24 21:48:57 -0400

    formatted

--- src/masonite/stubs/notification/Notification.py
+++ src/masonite/stubs/notification/Notification.py
@@ -5,8 +5,7 @@ from masonite.mail import Mailable
 class __class__(Notification, Mailable):
     def to_mail(self, notifiable):
         return (
-            self
-            .to(notifiable.email)
+            self.to(notifiable.email)
             .subject("Masonite 4")
             .from_("hello@email.com")
             .text(f"Hello {notifiable.name}")
