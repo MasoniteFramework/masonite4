ee8576b4ee269e53eac4c13b82df44a42ab3acdf
Samuel Girardin <samuelgirardin@pm.me>
2021-06-29 21:49:25 +0200

    remove duplicated register_storage() method

--- tests/integrations/app/Kernel/Kernel.py
+++ tests/integrations/app/Kernel/Kernel.py
@@ -92,19 +92,6 @@ class Kernel:
     def register_templates(self):
         self.application.bind("views.location", "tests/integrations/templates")
 
-    def register_storage(self):
-        storage = StorageCapsule(self.application.base_path)
-        storage.add_storage_assets(
-            {
-                # folder                             # template alias
-                "tests/integrations/storage/static": "static/",
-                "tests/integrations/storage/compiled": "static/",
-                "tests/integrations/storage/uploads": "static/",
-                "tests/integrations/storage/public": "/",
-            }
-        )
-        self.application.bind("storage_capsule", storage)
-
     def register_database(self):
         from masoniteorm.query import QueryBuilder
 
