ec2100bdf0b93260c711269c229dc3200100365e
Joe Mancuso <idmann509@gmail.com>
2021-10-25 19:18:32 -0400

    fixed cookie throwing errors

--- src/masonite/middleware/route/EncryptCookies.py
+++ src/masonite/middleware/route/EncryptCookies.py
@@ -4,12 +4,18 @@ from ...exceptions import InvalidToken
 class EncryptCookies:
     def before(self, request, response):
         for _, cookie in request.cookie_jar.all().items():
-            cookie.value = request.app.make("sign").unsign(cookie.value)
+            try:
+                cookie.value = request.app.make("sign").unsign(cookie.value)
+            except InvalidToken:
+                pass
 
         return request
 
     def after(self, request, response):
         for _, cookie in response.cookie_jar.all().items():
-            cookie.value = request.app.make("sign").sign(cookie.value)
+            try:
+                cookie.value = request.app.make("sign").sign(cookie.value)
+            except InvalidToken:
+                pass
 
         return request
