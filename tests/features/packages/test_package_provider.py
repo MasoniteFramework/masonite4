23afe31f4518676c964ed0d5fe9ca3bfe2327f71
Samuel Girardin <samuelgirardin@pm.me>
2021-12-22 00:11:18 +0100

    remove debug statements and unused comments

--- tests/features/packages/test_package_provider.py
+++ tests/features/packages/test_package_provider.py
@@ -10,12 +10,6 @@ class TestPackageProvider(TestCase):
     def test_config_is_merged(self):
         self.assertEqual(config("test_package.param_1"), 0)
 
-    # def test_package_config_can_be_published(self):
-    #     pp = self.application.providers[-1]
-    #     import pdb
-
-    #     pdb.set_trace()
-
     def test_views_are_registered(self):
         self.application.make("view").exists("test_package:package")
         self.application.make("view").exists("test_package:admin.settings")
@@ -33,10 +27,6 @@ class TestPackageProvider(TestCase):
         self.craft("test_package:command2").assertSuccess()
 
     def test_routes_are_registered(self):
-        # nb = len(self.application.make("router").routes)
-        # import pdb
-
-        # pdb.set_trace()
         for r in self.application.make("router").routes:
             print(r)
         self.get("/package/test/").assertContains("index")
